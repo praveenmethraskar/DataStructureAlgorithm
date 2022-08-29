d2874dd40643fa3baed71c06333a58d6772d2773
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-29 17:17:04 +0530

    permutation of string

---
+++ DataStructureAlgorithms/PermutationsOfString.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithms
+{
+    class PermutationsOfString
+    {
+        // Using HashSet collection for storing permutations of string uniquely.
+        HashSet<string> set = new HashSet<string>();
+        public void permutations()
+        {
+            Console.Write("Enter a string:  ");
+            string str = Console.ReadLine();
+
+            string result = "";
+            permute(str, result);
+
+            Console.Write("\nPermutations of string {0} are: \n", str);
+            foreach (var s in set)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        void permute(string str, string result)
+        {
+
+            if (str.Length == 0)
+            {
+                set.Add(result);
+                return;
+            }
+            for (int i = 0; i < str.Length; i++)
+            {
+                char ch = str[i];
+                String left_substr = str.Substring(0, i);
+                String right_substr = str.Substring(i + 1);
+                String rest = left_substr + right_substr;
+                permute(rest, result + ch);
+            }
+        }
+    }
+}
---
+++ DataStructureAlgorithms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DataStructureAlgorithms
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string flag = "Y";
+            while (flag == "Y" || flag == "y")
+            {
+                Console.WriteLine("Welcome To Datastructures and Algorithms");
+                Console.WriteLine("1.Permutation Using Recursion");
+                Console.WriteLine("Enter Your Option 1.permutation using recursion");
+                int num = Convert.ToInt32(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        PermutationsOfString permutationsofstring = new PermutationsOfString();
+                        permutationsofstring.permutations();
+                        break;
+
+                    default:
+                        Console.WriteLine("-------Enter the valid option---");
+                        break;
+                }
+                Console.WriteLine("\nDo you want to continue");
+                flag = Console.ReadLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
